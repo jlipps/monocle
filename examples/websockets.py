e4e30b93014fdcf8338b4838a860232c17e59a04
Jonathan Lipps <jlipps@gmail.com>
2012-03-07 18:37:14 -0800

    added WebSocketServer to tornado/http
    - along with examples/websockets.py to demonstrate

---
+++ examples/websockets.py
@@ -0,0 +1,54 @@
+import sys
+import new
+import simplejson
+
+import monocle
+from monocle import _o, Return
+monocle.init(sys.argv[1])
+
+from monocle.stack import eventloop
+from monocle.stack.network import add_service, Service
+from monocle.stack.network.http import HttpHeaders, WebSocketServer
+
+SOCKET_HTML = """
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>WebSocket Echo</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <h1>WebSocket Reverse Echo</h1>
+    <section>Your message backwards: <span id="content"></span></section>
+    <input id="message" type="text" style="margin: 20px 0px;"/>
+    <section>(You typed: <span id="realContent"></span>)</section>
+
+    <script>
+        var ws = new WebSocket('ws://localhost:8088/ws/');
+        ws.onopen = function(event) { console.log("Opened socket conn"); };
+        ws.onclose = function(event) { console.log("Closed socket conn"); };
+        ws.onmessage = function(event) {
+            $('#content').prepend(event.data);
+            $('#realContent').append(event.data);
+        };
+        $('#message').keyup(function() {
+            ws.send($(this).val());
+            $(this).val('');
+        });
+    </script>
+  </body>
+</html>
+"""
+
+@_o
+def web_handler():
+    yield Return(SOCKET_HTML)
+
+@_o
+def websocket_handler(message):
+    yield Return(message)
+
+
+add_service(WebSocketServer(web_handler, websocket_handler, 8088))
+eventloop.run()
--- monocle/tornado_stack/network/http.py
+++ monocle/tornado_stack/network/http.py
@@ -6,12 +6,13 @@ import urlparse
 
 import tornado.httpclient
 import tornado.httpserver
+import tornado.web
+import tornado.websocket
 
 from monocle import _o, Return, VERSION, launch
 from monocle.callback import Callback
 from monocle.stack.network import Client
 from monocle.stack.network.http import HttpHeaders, HttpClient
-
 
 class HttpException(Exception): pass
 
@@ -28,7 +29,6 @@ class HttpClient(HttpClient):
         _http_client.fetch(req, cb)
         response = yield cb
         yield Return(response)
-
 
 class HttpServer(object):
     def __init__(self, handler, port):
@@ -54,4 +54,37 @@ class HttpServer(object):
             _handler,
             io_loop=el._tornado_ioloop)
         self._http_server.listen(self.port)
+
+class WebSocketServer(HttpServer):
+    def __init__(self, web_handler, ws_handler, port):
+        self.web_handler = web_handler
+        self.ws_handler = ws_handler
+        self.port = port
+
+    def _add(self, el):
+        web_handler = self.web_handler
+        @_o
+        def _get(self):
+            content = yield launch(web_handler)
+            self.write(content)
+        WebHandler = type('WebHandler', (
+            tornado.web.RequestHandler,),
+            {'get': _get})
+
+        ws_handler = self.ws_handler
+        @_o
+        def _on_message(self, message):
+            response = yield launch(ws_handler, message)
+            self.write_message(response)
+        WSHandler = type('WSHandler',
+            (tornado.websocket.WebSocketHandler,),
+            {'on_message': _on_message})
+
+        self._tornado_app = tornado.web.Application([
+            (r'^/', WebHandler),
+            (r'^/ws/', WSHandler)])
+        self._http_server = tornado.httpserver.HTTPServer(
+            self._tornado_app, io_loop=el._tornado_ioloop)
+        self._http_server.listen(self.port)
+
 
